f335959c7b76a9ff1afa5da560a8cc030a1bac05
Arthur de Morais Novaes <arthurnovaes0@gmail.com>
2019-05-30 16:12:18 -0300

    finalizando prática

--- prog.c
+++ prog.c
@@ -7,10 +7,7 @@
 #include <fcntl.h>
 #include <stdint.h>
 
-// ORIG_BUFFER will be placed in memory and will then be changed to NEW_BUFFER
-// They must be the same length
-#define ORIG_BUFFER "Arthu e Gustavo"
-#define NEW_BUFFER "Hello, Linux!"
+#define ORIG_BUFFER "Arthur e Gustavo"
 
 // The page frame shifted left by PAGE_SHIFT will give us the physcial address of the frame
 // Note that this number is architecture dependent. For me on x86_64 with 4096 page sizes,
@@ -21,14 +18,16 @@
 
 void* create_buffer(void);
 unsigned long get_page_frame_number_of_address(void *addr);
-int open_memory(void);
-void seek_memory(int fd, unsigned long offset);
 
 int main(void) {
-   // Create a buffer with some data in it
+
+   // Cria o buffer e armazena na variável
    void *buffer = create_buffer();
    printf ("Buffer: %s\n", buffer);
+
+   // Mostra o endereço lógico do buffer
    printf("Endereço logico do buffer: %p\n\n", (unsigned long)buffer);
+
 
    // Get the page frame the buffer is on
    unsigned int page_frame_number = get_page_frame_number_of_address(buffer);
@@ -38,33 +37,11 @@ int main(void) {
    unsigned int distance_from_page_boundary = (unsigned long)buffer % getpagesize();
 
    // Determine how far to seek into memory to find the buffer
-   uint64_t offset = (page_frame_number << PAGE_SHIFT) + distance_from_page_boundary;
+   uint64_t physc = (page_frame_number << PAGE_SHIFT) + distance_from_page_boundary;
 
-   printf("Endereço físico: 0x%x\n", offset);
+   printf("Endereço físico: 0x%x\n", physc);
 
-   // Open /dev/mem, seek the calculated offset, and
-   // map it into memory so we can manipulate it
-   // CONFIG_STRICT_DEVMEM must be disabled for this
-   int mem_fd = open_memory();
-   seek_memory(mem_fd, offset);
-
-   printf("Buffer: %s\n", buffer);
-   puts("Changing buffer through /dev/mem...");
-   // Change the contents of the buffer by writing into /dev/mem
-   // Note that since the strings are the same length, there's no purpose in
-   // copying the NUL terminator again
-   if(write(mem_fd, NEW_BUFFER, strlen(NEW_BUFFER)) == -1) {
-      fprintf(stderr, "Write failed: %s\n", strerror(errno));
-   }
-
-   printf("Buffer: %s\n", buffer);
-
-   // Clean up
    free(buffer);
-   close(mem_fd);
-
-   return 0;
-  
 
    return 0;
 }
@@ -98,8 +75,8 @@ unsigned long get_page_frame_number_of_address(void *addr) {
    FILE *pagemap = fopen("/proc/self/pagemap", "rb");
 
    // Seek to the page that the buffer is on
-   unsigned long offset = (unsigned long)addr / getpagesize() * PAGEMAP_LENGTH;
-   if(fseek(pagemap, (unsigned long)offset, SEEK_SET) != 0) {
+   unsigned long physc = (unsigned long)addr / getpagesize() * PAGEMAP_LENGTH;
+   if(fseek(pagemap, (unsigned long)physc, SEEK_SET) != 0) {
       fprintf(stderr, "Failed to seek pagemap to proper location\n");
       exit(1);
    }
@@ -113,25 +90,4 @@ unsigned long get_page_frame_number_of_address(void *addr) {
    fclose(pagemap);
 
    return page_frame_number;
-}
-
-int open_memory(void) {
-   // Open the memory (must be root for this)
-   int fd = open("/dev/mem", O_RDWR);
-
-   if(fd == -1) {
-      fprintf(stderr, "Error opening /dev/mem: %s\n", strerror(errno));
-      exit(1);
-   }
-
-   return fd;
-}
-
-void seek_memory(int fd, unsigned long offset) {
-   unsigned pos = lseek(fd, offset, SEEK_SET);
-
-   if(pos == -1) {
-      fprintf(stderr, "Failed to seek /dev/mem: %s\n", strerror(errno));
-      exit(1);
-   }
 }
